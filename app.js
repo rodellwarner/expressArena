b64379e6c6e375c4c6131aba78a7b2beca4e9b6b
Rodell Warner <rodell.warner@gmail.com>
2020-12-16 18:00:50 -0500

    more exercises and routes added

--- app.js
+++ app.js
@@ -191,6 +191,74 @@ app.get("/lotto", (req, res) => {
   checkResults(lottoNumbers, selectedNumbers);
 });
 
+app.get("/hello", (req, res) => {
+  res.status(204).end();
+});
+
+app.get("/video", (req, res) => {
+  const video = {
+    title: "Cats falling over",
+    description: "15 minutes of hilarious fun as cats fall over",
+    length: "15:40",
+  };
+  res.json(video);
+  // res.send(video);
+});
+
+app.get("/colors", (req, res) => {
+  const colors = [
+    {
+      name: "red",
+      rgb: "FF0000",
+    },
+    {
+      name: "green",
+      rgb: "00FF00",
+    },
+    {
+      name: "blue",
+      rgb: "0000FF",
+    },
+  ];
+  res.json(colors);
+});
+
+app.get("/grade", (req, res) => {
+  // get the mark from the query
+  const { mark } = req.query;
+
+  // do some validation
+  if (!mark) {
+    // mark is required
+    return res.status(400).send("Please provide a mark");
+  }
+
+  const numericMark = parseFloat(mark);
+  if (Number.isNaN(numericMark)) {
+    // mark must be a number
+    return res.status(400).send("Mark must be a numeric value");
+  }
+
+  if (numericMark < 0 || numericMark > 100) {
+    // mark must be in range 0 to 100
+    return res.status(400).send("Mark must be in range 0 to 100");
+  }
+
+  if (numericMark >= 90) {
+    return res.send("A");
+  }
+
+  if (numericMark > 80) {
+    return res.send("B");
+  }
+
+  if (numericMark >= 70) {
+    return res.send("C");
+  }
+
+  res.send("F");
+});
+
 app.listen(8000, () => {
   console.log("Express server is listening on port 8000!");
 });
